81c3b57548b61e0890e44797dce284c56649680e
Ankur Chaprana <ankur.chaprana4886gmail.com>
2023-09-21 12:38:12 +0530

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PropertyListComponent } from './Property/property-list/property-list.component';
+import { PropertyCardComponent } from './Property/property-card/property-card.component';
+import { NavBarComponent } from './Property/nav-bar/nav-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PropertyListComponent,
+    PropertyCardComponent,
+    NavBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
